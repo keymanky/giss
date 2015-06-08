f8c2ff51f1a2c9e6e8f81ff875fbbee495975cac
Jorge Salgado Win <keymanky@hotmail.com>
2015-06-08 11:35:57 -0500

    Agrego todo lo del cuestionario

--- public/controllers/administrativo.js
+++ public/controllers/administrativo.js
@@ -1,6 +1,6 @@
 //Cerebro de angular donde controlaremos el el funcionamiento, es decir aqui ejecutarmeos las acciones importantes
 
-angular.module('administrativo_middle',[])
+angular.module('administrativo_middle',['youtube-embed'])
 	.controller('administrativo_page', [ '$scope', 'giss_servicios' , function ($scope, giss_servicios){
 		$scope.json = {};
 
@@ -20,6 +20,7 @@ angular.module('administrativo_middle',[])
 
 			giss_servicios.consultar_id_cuestionario(id).success( function(data){
 				$scope.cuestionarioseleccionado = data;
+				console.log($scope.url_cuestionario);
 			});
 		}
 
